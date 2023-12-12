9c4719d2ed270b9086b9437ba20b97cace8094be
rastylus <ryan.p.riggs@gmail.com>
2023-12-12 08:21:03 -0600

    added get by id

--- happyroots-react/src/Controllers/FavoritesController.js
+++ happyroots-react/src/Controllers/FavoritesController.js
@@ -4,7 +4,7 @@ const baseUrl = "http://localhost:8080/favorites/"
 const handleResponse = async (response) => {
     if(response.ok) {
         const data = await response.json()
-        console.log('data', data)
+        // console.log('data', data)
         return data
     } else return false;
 }
@@ -25,7 +25,7 @@ export const getFavorites =  async (token, userId) => {
 
 
 
-export const getAllFavorites =  async (token, userId) => {
+export const getAll =  async (token, userId) => {
 
     const response = await fetch(baseUrl + "all", {
         method: 'GET',
@@ -36,3 +36,45 @@ export const getAllFavorites =  async (token, userId) => {
     })
     return handleResponse(response)
 }
+
+export const getAllFavorites =  async (token, userId) => {
+
+    const response = await fetch(baseUrl + "allfavorites", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+    return handleResponse(response)
+}
+
+const addFave = '{"id" : "6", "name" : "dummy", "userId" : "fh3947fhweiuhf09w8"}';
+
+
+export const addFavorites =  async (token, addFave) => {
+
+    const response = await fetch(baseUrl + "add", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    })
+    return handleResponse(response)
+}
+
+
+// export const addFavorites = (token, data) => {
+//     fetch(baseUrl + "add", {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Authorization': `Bearer ${token}`
+
+//         },
+//         body: JSON.stringify(data),
+//     }).then(response => {
+//         return response.data
+//     });
+// }
