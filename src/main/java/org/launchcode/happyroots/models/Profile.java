5e08d2b868dae1c04a40e9056b91fce6c30b3743
rastylus <ryan.p.riggs@gmail.com>
2023-12-15 00:53:10 -0600

    need just adjust for join columns

--- src/main/java/org/launchcode/happyroots/models/Profile.java
+++ src/main/java/org/launchcode/happyroots/models/Profile.java
@@ -2,9 +2,15 @@ package org.launchcode.happyroots.Models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -12,6 +18,17 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Profile extends AbstractEntity {
 
+//
+//    @OneToMany
+//    @JoinColumn(name = "favorite_id")
+    private List<Plant> plants = new ArrayList<>();
+
+
+    private String name;
     private String userId;
+    private String email;
+    private String phoneNumber;
+    private String imageURL;
+
 
 }
