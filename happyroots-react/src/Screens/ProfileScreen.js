9033d9430b29b2a8a752d4a2f4b83860461ac217
Artur Uvarov <artur.uff@gmail.com>
2023-12-14 23:35:18 -0500

    user customization partly complete

--- happyroots-react/src/Screens/ProfileScreen.js
+++ happyroots-react/src/Screens/ProfileScreen.js
@@ -67,10 +67,14 @@ const ProfileScreen = () => {
               <p>Email: {user.email}</p>
               <p>Username: {user.displayName}</p>
       
-              {user.photoURL && (
-                <img className="user-info-img" src={user.photoURL} alt="User Avatar" />
-              )}
-      
+              <img 
+                className="user-info-img" 
+                src={user.photoURL 
+                  ? user.photoURL 
+                  : 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'} 
+                alt="User Avatar" 
+              />
+              
               {isEditMode ? (
                 <>
                   <label className="edit-mode-label">
