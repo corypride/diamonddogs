d1b70d3e2b07ec4919ac4718358beaa4338e5255
collinaustin <collinaustin@gmail.com>
2023-12-18 11:47:57 -0600

    Added files to .gitignore and removed unneeded security dependencies.

--- src/main/java/org/launchcode/happyroots/HappyrootsApplication.java
+++ src/main/java/org/launchcode/happyroots/HappyrootsApplication.java
@@ -2,9 +2,8 @@ package org.launchcode.happyroots;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class HappyrootsApplication {
 
 	public static void main(String[] args) {
