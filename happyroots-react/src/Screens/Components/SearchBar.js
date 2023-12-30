22471151d66fa05afbd03fd92e585bfc0ab721f3
forsythet@gmail.com <forsythet@gmail.com>
2023-12-30 12:03:39 -0600

    Changed API key to allow more API requests, allow user to see search request of common names from plant list

--- happyroots-react/src/Screens/Components/SearchBar.js
+++ happyroots-react/src/Screens/Components/SearchBar.js
@@ -1,24 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {FaSearch} from "react-icons/fa";
 import "../styles/SearchBar.css";
 import { IoMdClose } from "react-icons/io";
+import axios from 'axios';
 
 const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("")
 
   const fetchData = (value) => {
     // The line below is a placeholder URL for the API we want to fetch data from. Link to Perenual API.
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((json) => {
-      const results = json.filter((user) => {
+    //fetch("https://jsonplaceholder.typicode.com/users")
+    fetch("https://perenual.com/api/species-list?key=sk-p0RY6572ddd57bba23207")
+      .then((response) => response.json())
+      .then((json) => {
+      const results = json.data.filter((plant) => {
         return (
           value &&
-          user && 
-          user.name && 
-          user.name.toLowerCase().includes(value)
+          plant &&
+          plant.common_name && 
+          plant.common_name.toLowerCase().includes(value)
         );
       });
+      console.log(json);
       setResults(results);
     });
   }
@@ -28,7 +31,6 @@ const SearchBar = ({ setResults }) => {
     fetchData(value)
   }
 
-
   const clearInput = () => {
     setInput("");
     setResults([])
@@ -37,7 +39,7 @@ const SearchBar = ({ setResults }) => {
   return (
     <div className='input-wrapper'>
       <input 
-        placeholder="Type to search..." 
+        placeholder="Search plant names..." 
         value={input} 
         onChange={(e) => handleChange(e.target.value)} 
       />
@@ -53,4 +55,3 @@ const SearchBar = ({ setResults }) => {
 };
 
 export default SearchBar;
-
--- happyroots-react/src/Screens/Components/SearchResult.js
+++ happyroots-react/src/Screens/Components/SearchResult.js
@@ -3,7 +3,7 @@ import "../styles/SearchResult.css";
 
 // Modify result to match paramter from Perenual API. Below is just a placeholder.
 const SearchResult = ({ result }) => {
-  return <div className="search-result">{result.name}</div>
+  return <div className="search-result">{result.common_name}</div>
 };
 
 export default SearchResult;
