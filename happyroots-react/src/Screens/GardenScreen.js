3d0f153dbb7518ba8bae70e2d1f639cddd44919a
rastylus <ryan.p.riggs@gmail.com>
2023-12-30 02:34:38 -0600

    added loading and error states

--- happyroots-react/src/Screens/GardenScreen.js
+++ happyroots-react/src/Screens/GardenScreen.js
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from "react";
 import NavigationBar from "./Components/NavigationBar";
-import "../App.css";
 import { getUserFavorites } from "../Controllers/FavoritesController";
 import SpeciesDisplay from "./Components/SpeciesDisplay";
+import "../App.css";
 
 const GardenScreen = () => {
-  const [faves, setFaves] = useState([]);
+  const [status, setStatus] = useState({
+    loading: true,
+    error: null,
+    data: [],
+  });
 
   useEffect(() => {
-  fetchUserFavorites();
+    fetchUserFavorites();
   }, []);
 
   const fetchUserFavorites = async () => {
-    const responseData = await getUserFavorites();
-    if (responseData) {
-      return setFaves(responseData)
+    try {
+      const responseData = await getUserFavorites();
+      setStatus({ loading: false, error: null, data: responseData });
+    } catch (error) {
+      console.error("Error fetching favorites:", error);
+      setStatus({
+        loading: false,
+        error: "Error fetching favorites. Please try again.",
+        data: [],
+      });
     }
   };
 
@@ -22,9 +33,22 @@ const GardenScreen = () => {
     <>
       <NavigationBar />
       <div>
-        {faves.map((fave) => <SpeciesDisplay fave={fave} refresh={() => fetchUserFavorites()}/>)}
+        {status.loading ? (
+          <p>Loading favorites...</p>
+        ) : status.error ? (
+          <p>{status.error}</p>
+        ) : (
+          status.data.map((fave) => (
+            <SpeciesDisplay
+              key={fave.id}
+              fave={fave}
+              refresh={() => fetchUserFavorites()}
+            />
+          ))
+        )}
       </div>
     </>
   );
 };
+
 export default GardenScreen;
