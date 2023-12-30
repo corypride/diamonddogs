d750422424b317b8f97fbcde0298b2e39e5df82b
rastylus <ryan.p.riggs@gmail.com>
2023-12-29 23:47:57 -0600

    added conditional link for login/logout

--- happyroots-react/src/Screens/Components/NavigationBar.js
+++ happyroots-react/src/Screens/Components/NavigationBar.js
@@ -7,42 +7,56 @@ import { login, logout } from '../../Controllers/AuthController';
 import { useEffect, useState } from 'react';
 import { getUserFromLocalStorage } from '../../Helpers/authHelpers';
 
-
-
-
 const NavigationBar = () => {
-
-  
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
-
-
 
   useEffect(() => {
     const user = getUserFromLocalStorage();
 
-    if (user) {
-        // console.log('User:', user);
-        setUser(user.providerData[0]);
+    if (!user) {
+      console.log("User not logged in");
+      navigate("/login");
+    } else if (user.providerData && user.providerData.length > 0) {
+      console.log("User:", user);
+      setUser(user.providerData[0]);
     } else {
-        // console.log('User not logged in');
-        navigate('/login');
+      console.log("User data not available");
     }
-}, []);
-
-
+  }, []);
 
   return (
     <nav>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/search">Search</Link></li>
-        <li><Link to="/browse">Browse</Link></li>
-        <li><Link to="/favorites">Favorites</Link></li>
-        <li><Link to="/garden">Garden</Link></li>
-        <li><Link to="/profile">Profile</Link></li>
-        {/* <li><Link to="/login">Login</Link> </li> */}
-        <li><Link to="/login" onClick={(logout)}>Logout</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/search">Search</Link>
+        </li>
+        <li>
+          <Link to="/browse">Browse</Link>
+        </li>
+        <li>
+          <Link to="/favorites">Favorites</Link>
+        </li>
+        <li>
+          <Link to="/garden">Garden</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        {!user ? (
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        ) : (
+          <li>
+            <Link to="/" onClick={logout}>
+              Logout
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
