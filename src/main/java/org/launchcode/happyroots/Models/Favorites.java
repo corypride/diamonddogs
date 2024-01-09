7ea4495847980f41adf180ddbb378445bed672aa
rastylus <ryan.p.riggs@gmail.com>
2024-01-09 03:26:24 -0600

    added unique constraint to favorite model instead of manually creating index in mysql

--- src/main/java/org/launchcode/happyroots/Models/Favorites.java
+++ src/main/java/org/launchcode/happyroots/Models/Favorites.java
@@ -1,23 +1,32 @@
 package org.launchcode.happyroots.Models;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.SQLInsert;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-
-@Table(name = "favorites")
+@Table(name = "favorites", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"userId", "speciesId"})})
 public class Favorites extends AbstractEntity{
 
+    @Column(name = "user_id")
     private String userId;
-    private String commonName;
+
+    @Column(name = "species_id")
     private int speciesId;
 
+    private String commonName;
+
 }
+
+
+
+
+
