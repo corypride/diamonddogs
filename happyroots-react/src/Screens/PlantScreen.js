3c937956d26b2797589ecce951b10fb7d004fa2f
collinaustin <collinaustin@gmail.com>
2024-01-10 17:44:41 -0600

    Added more details to plant page.

--- happyroots-react/src/Screens/PlantScreen.js
+++ happyroots-react/src/Screens/PlantScreen.js
@@ -32,12 +32,15 @@ const PlantScreen = () => {
     <div>
       <NavigationBar />
       <h1>{plant.common_name}</h1>
+      <h4>Scientific Name: {plant.scientific_name}</h4>
       <img src={plant.default_image?.medium_url} alt={`Image of ${plant.common_name}`} />
       <p>Description: {plant.description}</p>
-      <p>Scientific Name: {plant.scientific_name}</p>
       {plant.family != null && <p>Family: {plant.family}</p>}
       <p>Origin: {plant.origin.join(', ')}</p>
       <p>Type: {plant.type}</p>
+      <p>Indoor plant: {plant.indoor ? "Yes" : "No"}</p>
+      <p>Max growth height: {plant.dimensions.max_value}</p>
+      <p>Growth rate: {plant.growth_rate}</p>
       <p>Growth cycle: {plant.cycle}</p>
       <p>Edible Fruits: {plant.edible_fruit ? "Yes" : "No"}</p>
       <p>Flowers: {plant.flowers ? "Yes" : "No"}</p>
@@ -48,6 +51,9 @@ const PlantScreen = () => {
       <p>Watering Information: {careInfo.wateringDesc}</p>
       <p>Sunlight Information: {careInfo.sunlightDesc}</p>
       <p>Pruning Information: {careInfo.pruningDesc}</p>
+      {plant.pest_susceptibility && plant.pest_susceptibility.length > 0 && (
+      <p>Pest Susceptibility: {plant.pest_susceptibility.join(', ')}</p>)}
+
 
       <h3>{plant.common_name} Hardiness Map</h3>
       <p>Hardiness Range: Min: {plant.hardiness.min} - Max: {plant.hardiness.max}</p>
